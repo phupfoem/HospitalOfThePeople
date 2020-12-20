cbd95d2f963a1709af5522bbcb3c764f8e3faf9e
hnamng2301 <75629713+hnamng2301@users.noreply.github.com>
2020-12-20 17:04:24 +0700

    Add files via upload

---
+++ FmSur.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace HospitalOfThePeople
+{
+    partial class FmSur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSNo = new System.Windows.Forms.TextBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtFee = new System.Windows.Forms.TextBox();
+            this.txtRNo = new System.Windows.Forms.TextBox();
+            this.lblSNo = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblFee = new System.Windows.Forms.Label();
+            this.lblRNo = new System.Windows.Forms.Label();
+            this.btnAdmit = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSNo
+            // 
+            this.txtSNo.Location = new System.Drawing.Point(57, 79);
+            this.txtSNo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSNo.Name = "txtSNo";
+            this.txtSNo.Size = new System.Drawing.Size(254, 20);
+            this.txtSNo.TabIndex = 11;
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(89, 54);
+            this.txtType.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(144, 20);
+            this.txtType.TabIndex = 8;
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(141, 28);
+            this.txtTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(92, 20);
+            this.txtTime.TabIndex = 5;
+            // 
+            // txtFee
+            // 
+            this.txtFee.Location = new System.Drawing.Point(46, 28);
+            this.txtFee.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtFee.Name = "txtFee";
+            this.txtFee.Size = new System.Drawing.Size(92, 20);
+            this.txtFee.TabIndex = 4;
+            // 
+            // txtRNo
+            // 
+            this.txtRNo.Location = new System.Drawing.Point(326, 2);
+            this.txtRNo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtRNo.Name = "txtRNo";
+            this.txtRNo.Size = new System.Drawing.Size(22, 20);
+            this.txtRNo.TabIndex = 2;
+            // 
+            // lblSNo
+            // 
+            this.lblSNo.Location = new System.Drawing.Point(0, 0);
+            this.lblSNo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSNo.Name = "lblSNo";
+            this.lblSNo.Size = new System.Drawing.Size(75, 19);
+            this.lblSNo.TabIndex = 46;
+            // 
+            // lblType
+            // 
+            this.lblType.Location = new System.Drawing.Point(0, 0);
+            this.lblType.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(75, 19);
+            this.lblType.TabIndex = 47;
+            // 
+            // lblTime
+            // 
+            this.lblTime.Location = new System.Drawing.Point(0, 0);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(75, 19);
+            this.lblTime.TabIndex = 48;
+            // 
+            // lblFee
+            // 
+            this.lblFee.Location = new System.Drawing.Point(0, 0);
+            this.lblFee.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFee.Name = "lblFee";
+            this.lblFee.Size = new System.Drawing.Size(75, 19);
+            this.lblFee.TabIndex = 45;
+            // 
+            // lblRNo
+            // 
+            this.lblRNo.Location = new System.Drawing.Point(0, 0);
+            this.lblRNo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRNo.Name = "lblRNo";
+            this.lblRNo.Size = new System.Drawing.Size(75, 19);
+            this.lblRNo.TabIndex = 49;
+            // 
+            // btnAdmit
+            // 
+            this.btnAdmit.Location = new System.Drawing.Point(25, 156);
+            this.btnAdmit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAdmit.Name = "btnAdmit";
+            this.btnAdmit.Size = new System.Drawing.Size(111, 37);
+            this.btnAdmit.TabIndex = 8;
+            this.btnAdmit.Text = "Admit";
+            this.btnAdmit.UseVisualStyleBackColor = true;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(140, 156);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(111, 37);
+            this.btnCheck.TabIndex = 9;
+            this.btnCheck.Text = "Find";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(140, 211);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(111, 37);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(255, 155);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(93, 38);
+            this.btnUpdate.TabIndex = 15;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(352, 155);
+            this.btnFind.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(106, 38);
+            this.btnFind.TabIndex = 17;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(57, 105);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(438, 20);
+            this.txtUsername.TabIndex = 12;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(4, 107);
+            this.lblUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.TabIndex = 42;
+            this.lblUsername.Text = "Username:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(57, 131);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(438, 20);
+            this.txtPassword.TabIndex = 13;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(4, 133);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 44;
+            this.lblPassword.Text = "Password:";
+            // 
+            // FmSur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 210);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtFee);
+            this.Controls.Add(this.lblFee);
+            this.Controls.Add(this.txtSNo);
+            this.Controls.Add(this.lblSNo);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.txtRNo);
+            this.Controls.Add(this.lblRNo);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnAdmit);
+            this.Controls.Add(this.btnFind);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FmSur";
+            this.Text = "Surgery Management";
+            this.Load += new System.EventHandler(this.FmEmp_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSNo;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtFee;
+        private System.Windows.Forms.TextBox txtRNo;
+        private System.Windows.Forms.Label lblSNo;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblFee;
+        private System.Windows.Forms.Label lblRNo;
+        private System.Windows.Forms.Button btnAdmit;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+
+
+    }
+}
